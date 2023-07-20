6a5f84efe6676561989c1ebe328b0d30cf10c67b
Israel <israelfelipe9@gmail.com>
2023-07-20 16:52:15 -0300

    feat: add quick search

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import QuickSeach from "./components/QuickSeach";
 import TripSearch from "./components/TripSearch";
 
 export default function Home() {
   return (
     <div>
       <TripSearch />
+      <QuickSeach />
     </div>
   );
 }
