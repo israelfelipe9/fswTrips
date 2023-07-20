550a16b4765073761d98185f152ea5a726478911
Israel <israelfelipe9@gmail.com>
2023-07-20 16:07:29 -0300

    feat: add input , datepicker and currency input components

---
+++ src/components/Header.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { signIn, signOut, useSession } from "next-auth/react";
+import Image from "next/image";
+import React, { useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+
+const Header = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+  const { status, data } = useSession();
+
+  const handleLoginClick = () => signIn();
+
+  const handleLogoutClick = () => {
+    setMenuIsOpen(false);
+    signOut();
+  };
+
+  const handleMenuClick = () => setMenuIsOpen(!menuIsOpen);
+
+  return (
+    <div className="container mx-auto p-5 py-0 h-[93px] flex justify-between items-center">
+      <div className="relative h-[32px] w-[182px]">
+        <Image src="/logo.png" alt="FSW logos" fill />
+      </div>
+      {status === "unauthenticated" && (
+        <button
+          className="text-primary text-sm font-semibold"
+          onClick={handleLoginClick}
+        ></button>
+      )}
+      {status === "authenticated" && data.user && (
+        <div className="flex items-center gap-3 border-grayLighter border border-solid rounded-full p-2 relative">
+          <AiOutlineMenu
+            size={16}
+            onClick={handleMenuClick}
+            className="cursor-pointer"
+          />
+          <Image
+            src={data.user.image!}
+            height={35}
+            width={35}
+            alt={data.user.name!}
+            className="rounded-full shadow-md"
+          />
+          {menuIsOpen && (
+            <div className="absolute top-14 left-0 w-full h-full bg-white rounded-lg shadow-md flex flex-col justify-center items-center">
+              <button
+                className="text-primary text-xs font-semibold"
+                onClick={handleLogoutClick}
+              >
+                Logout
+              </button>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Header;
