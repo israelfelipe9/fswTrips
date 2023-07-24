165ff93b5dee5459a79a9ab95ea37f5a556d0716
Israel <israelfelipe9@gmail.com>
2023-07-24 10:08:10 -0300

    feat: add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-walterWhite p-5 justify-center flex flex-col items-center mt-1">
+      <Image src="/logo.png" alt="full stack week" width={133} height={23} />
+      <p className="text-sm font-medium text-primaryDarker">
+        Todos os direitos reservados.
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
