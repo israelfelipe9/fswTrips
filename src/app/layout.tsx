b29c87a61ddf8d84b55ff5d23ee6b98942ed66c7
Israel <israelfelipe9@gmail.com>
2023-07-24 23:39:28 -0300

    feat: add and configure react-toastify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import Header from "../components/Header";
 import Footer from "@/components/Footer";
+import ToastProvider from "@/providers/toast";
 
 const poppins = Poppins({
   subsets: ["latin"],
@@ -24,9 +25,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={poppins.className}>
         <NextAuthProvider>
-          <Header />
-          {children}
-          <Footer />
+          <ToastProvider>
+            <Header />
+            {children}
+            <Footer />
+          </ToastProvider>
         </NextAuthProvider>
       </body>
     </html>
