2b6f75c03a1c529e660aa0afb2027872bac46d4b
Israel <israelfelipe9@gmail.com>
2023-07-27 01:18:24 -0300

    style: adapt search page results to desktop

--- src/app/trips/search/page.tsx
+++ src/app/trips/search/page.tsx
@@ -33,24 +33,20 @@ const Trips = () => {
   }, []);
 
   return (
-    <div className="container mx-auto flex flex-col items-center p-5">
-      {trips?.length === 0 ? (
-        <h1 className="text-primaryDarker font-semibold text-xl">
-          Nenhuma hospedagem encontrada!
+    <div className="container mx-auto flex flex-col items-center lg:items-start p-5">
+      <div>
+        <h1 className="text-primaryDarker font-semibold text-xl lg:text-[2.5rem] lg:text-left">
+          Hospedagens encontradas
         </h1>
-      ) : (
-        <div>
-          <h1 className="text-primaryDarker font-semibold text-xl">
-            Hospedagens encontradas
-          </h1>
-          <h2 className="text-grayPrimary font-medium mb-5">
-            Listamos as melhores viagens para você!
-          </h2>
-          <div className="flex flex-col gap-4">
-            {trips?.map((trip) => <TripItem key={trip.id} trip={trip} />)}
-          </div>
+        <h2 className="text-grayPrimary font-medium mb-5 lg:mt-6 lg:w-full lg:text-left">
+          {trips.length > 0
+            ? "Listamos as melhores viagens para você!"
+            : "Não encontramos nenhuma viagem com seus parâmetros!"}
+        </h2>
+        <div className="flex flex-col gap-4 lg:grid lg:grid-cols-4 lg:gap-10 lg:mt-6 lg:pb-16">
+          {trips?.map((trip) => <TripItem key={trip.id} trip={trip} />)}
         </div>
-      )}
+      </div>
     </div>
   );
 };
